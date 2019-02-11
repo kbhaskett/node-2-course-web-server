898dac935ec5794f422fda8278dc800a1c587a63
Kevin Haskett <Kevin.Haskett@siriuscom.com>
2019-02-11 13:37:40 -0600

    getting ready for Heroku

--- server.js
+++ server.js
@@ -1,29 +1,57 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => {
-    res.send({
-        name: 'Kevin',
-        likes: ['Golf', 'Basketball']
-    });
-    console.log(__dirname);
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n');
+  next();
 });
 
-app.get('/help', (req, res) => {
-    res.send('<H1>This is the Help Page.</H1>');
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about.hbs');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
 });
 
+// /bad - send back json with errorMessage
 app.get('/bad', (req, res) => {
-    res.send("Bad things happened.")
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
